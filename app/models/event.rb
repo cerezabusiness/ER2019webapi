60298fc3ae4c22e4d9cba2b1afafb0fc4cee3363
Leonardo Linero <leonardoraulinero@gmail.com>
2019-05-06 15:10:59 -0500

    order by date

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   has_many :profiles, through: :persons_events
   has_many :multimedium
   has_many :information
-  has_many :event_dates
+  has_many :event_dates, -> { order(:date) }
   has_many :faqs
   has_many :places_events
   has_many :places, through: :places_events
