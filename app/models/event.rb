9e3843b54e9050f9f4d6cdf111f88dd0ead94e9d
Leonardo Linero <leonardoraulinero@gmail.com>
2019-05-11 11:16:24 -0500

    returns only exhibitors

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_many :persons_events
+  has_many :persons_events, -> { where profile_id: 2 }
   has_many :people, -> { order(:name) }, through: :persons_events
   has_many :profiles, through: :persons_events
   has_many :multimedia
