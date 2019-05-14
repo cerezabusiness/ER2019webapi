7536be3cb4912b1e8a287b1f2159f1aa4bfbfae4
Leonardo Linero <leonardoraulinero@gmail.com>
2019-05-14 01:37:03 -0500

    button to instead link to

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,6 @@ class Person < ApplicationRecord
   has_many :persons_events, dependent: :destroy
   has_many :events, through: :persons_events
   has_many :profiles, through: :persons_events
-  has_many :activities_people
+  has_many :activities_people, dependent: :destroy
   has_many :activities, through: :activities_people
 end
